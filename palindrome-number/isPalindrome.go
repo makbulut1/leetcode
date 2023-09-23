81b6590a7cc15d32bba6d486963f569ed7a91def
Mehmet Eren Akbulut <98176777+makbulut1@users.noreply.github.com>
2023-09-23 23:42:22 +0300

    Create isPalindrome.go

---
+++ palindrome-number/isPalindrome.go
@@ -0,0 +1,48 @@
+func length(x int) int {
+  len := 0
+  for ;x > 0; len++ {
+    x = x / 10
+  }
+  return len
+}
+
+func itoa(x int) string {
+    var len int
+    var ret []byte;
+    tmp := x
+    if x < 0{
+        x = x * -1
+        len = length(x) + 1
+        ret = make([]byte, len)    
+    }else{
+        len = length(x)
+        ret = make([]byte, len)
+    }
+    for i := len - 1; i >= 0; i-- {
+        ret[i] = byte((x % 10) + 48)
+        x = x / 10
+    }
+    if (tmp < 0){
+        ret[0] = '-'
+    }
+    return string(ret)
+}
+
+func rev(str string) string {
+    ret := make([]byte, len(str))
+    i := 0
+    for len := len(str); len > 0; len-- {
+        ret[i] = str[len - 1]
+        i++
+    }
+    return string(ret)
+}
+
+func isPalindrome(x int) bool {
+    array := itoa(x)
+    rearray := rev(itoa(x))
+    if array == rearray {
+        return true
+    }
+    return false
+}
